4bf2f6faa00ef6274be57c1077fefe5b30b1a348
example0 <mail@example0>
2018-04-16 12:19:02 -0300

    ajuste

--- src/components/list/listItem.js
+++ src/components/list/listItem.js
@@ -6,10 +6,15 @@ import { blueGrey400, white } from 'material-ui/styles/colors';
 export default class ListItemData extends React.Component {
     render() {
         return (
+            /*
             <ListItem primaryText={this.props.item.title + ": " + this.props.item.description}
                 key={this.props.item.title}
                 leftAvatar={<Avatar color={white} backgroundColor={blueGrey400}>{this.props.item.title}</Avatar>}>
             </ListItem>
+            */
+            <ListItem primaryText={this.props.item.title + ": " + this.props.item.description}
+            key={this.props.item.title}>
+        </ListItem>
         )
     }
 }
