659dc2f847660aa1adbe201bc72531540253d11c
laurareyeslarrosa <laurareyeslarrosa@gmail.com>
2018-04-18 21:15:10 -0300

    list item style

--- src/components/list/listItem.js
+++ src/components/list/listItem.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { ListItem } from 'material-ui/List';
-import Avatar from 'material-ui/Avatar';
-import { blueGrey400, white } from 'material-ui/styles/colors';
+
 
 export default class ListItemData extends React.Component {
     render() {
         return (
-            /*
-            <ListItem primaryText={this.props.item.title + ": " + this.props.item.description}
-                key={this.props.item.title}
-                leftAvatar={<Avatar color={white} backgroundColor={blueGrey400}>{this.props.item.title}</Avatar>}>
+            <ListItem
+                key={this.props.item.title}>
+                <Paper zDepth={2}>
+                    <div>{this.props.item.title}</div>
+                    
+                    <div>by: {this.props.item.user}</div>
+                    <div>
+                        <div>Category: {this.props.item.category}</div>
+                        <div>Complete: {this.props.item.percentage_complete}%</div>
+                        <div>Date from: {this.props.item.date_from}</div>
+                        <div>Date to: {this.props.item.date_from}</div>
+                    </div>
+                    <div>
+                        <div>Detalle</div>
+                        <div>{this.props.item.description}</div>
+                    </div>
+                </Paper>
             </ListItem>
-            */
-            <ListItem primaryText={this.props.item.title + ": " + this.props.item.description}
-            key={this.props.item.title}>
-        </ListItem>
         )
     }
 }
